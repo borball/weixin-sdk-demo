6cd3a9a65c48c68aed565ec1a3efa31468701f5d
borball <borball.zhai@ericsson.com>
2016-02-25 12:34:12 -0500

    update demo and weixin-sdk to 0.7.2

--- src/main/java/com/riversoft/weixin/demo/WxCallbackController.java
+++ src/main/java/com/riversoft/weixin/demo/WxCallbackController.java
@@ -4,19 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.riversoft.weixin.common.decrypt.AesException;
 import com.riversoft.weixin.common.decrypt.MessageDecryption;
 import com.riversoft.weixin.common.decrypt.SHA1;
-import com.riversoft.weixin.common.event.ClickEvent;
+import com.riversoft.weixin.common.event.EventRequest;
 import com.riversoft.weixin.common.message.XmlMessageHeader;
-import com.riversoft.weixin.common.message.xml.TextXmlMessage;
 import com.riversoft.weixin.common.util.XmlObjectMapper;
 import com.riversoft.weixin.mp.base.AppSetting;
+import com.riversoft.weixin.mp.care.CareMessages;
 import com.riversoft.weixin.mp.message.MpXmlMessages;
-import com.riversoft.weixin.mp.message.xml.Forward2CareXmlMessage;
-import com.riversoft.weixin.qy.base.AgentSetting;
-import com.riversoft.weixin.qy.base.DefaultSettings;
+import com.riversoft.weixin.mp.user.Users;
+import com.riversoft.weixin.qy.base.CorpSetting;
 import com.riversoft.weixin.qy.message.QyXmlMessages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +29,16 @@ public class WxCallbackController {
 
     private static Logger logger = LoggerFactory.getLogger(WxCallbackController.class);
 
+    /**
+     * token/aesKey 是和企业号应用绑定的，这里为了演示方便使用外部注入，实际使用的时候一个企业号可能有多个应用，这样的话需要有个分发逻辑。
+     * 比如callback url可以定义为  /wx/qy/[应用的ID]，通过ID查询不同的token和aesKey
+     */
+    @Value("${agent.default.token}")
+    private String token;
+
+    @Value("${agent.default.aesKey}")
+    private String aesKey;
+
     @Autowired
     private DuplicatedMessageChecker duplicatedMessageChecker;
 
@@ -38,6 +48,9 @@ public class WxCallbackController {
 
     /**
      * 企业号回调接口
+     * 这里为了演示方便使用单个URL，实际使用的时候一个企业号可能有多个应用，这样的话需要有个分发逻辑：
+     * 比如callback url可以定义为  /wx/qy/[应用的ID]，通过ID查询不同的token和aesKey
+     *
      * @param signature
      * @param timestamp
      * @param nonce
@@ -55,18 +68,23 @@ public class WxCallbackController {
 
         logger.info("msg_signature={}, nonce={}, timestamp={}, echostr={}", signature, nonce, timestamp, echostr);
 
-        AgentSetting agentSetting = DefaultSettings.defaultSettings().getAgentSetting();
-        String corpId = DefaultSettings.defaultSettings().getCorpSetting().getCorpId();
+        CorpSetting corpSetting = CorpSetting.defaultSettings();
 
         try {
-            MessageDecryption messageDecryption = new MessageDecryption(agentSetting.getToken(), agentSetting.getAesKey(), corpId);
+            MessageDecryption messageDecryption = new MessageDecryption(token, aesKey, corpSetting.getCorpId());
             if (!StringUtils.isEmpty(echostr)) {
                 String echo = messageDecryption.decryptEcho(signature, timestamp, nonce, echostr);
                 logger.info("消息签名验证成功.");
                 return echo;
             } else {
                 XmlMessageHeader xmlRequest = QyXmlMessages.fromXml(messageDecryption.decrypt(signature, timestamp, nonce, content));
-                qyDispatch(xmlRequest);
+                XmlMessageHeader xmlResponse = qyDispatch(xmlRequest);
+                if(xmlResponse != null) {
+                    try {
+                        return messageDecryption.encrypt(XmlObjectMapper.defaultMapper().toXml(xmlResponse), timestamp, nonce);
+                    } catch (JsonProcessingException e) {
+                    }
+                }
             }
         } catch (Exception e) {
             logger.error("callback failed.", e);
@@ -75,12 +93,19 @@ public class WxCallbackController {
         return "";
     }
 
-    private void qyDispatch(XmlMessageHeader xmlRequest) {
+    private XmlMessageHeader qyDispatch(XmlMessageHeader xmlRequest) {
+        //添加处理逻辑
 
+        //需要同步返回消息（被动响应消息）给用户则构造一个XmlMessageHeader类型，比较鸡肋，因为处理逻辑如果比较复杂响应太慢会影响用户感知，建议直接返回null；
+        //如果有消息需要发送给用户则可以调用主动消息发送接口进行异步发送
+        return null;
     }
 
     /**
      * 公众号回调接口
+     * 这里为了演示方便使用单个固定URL，实际使用的时候一个一个系统可能有多个公众号，这样的话需要有个分发逻辑：
+     * 比如callback url可以定义为  /wx/mp/[公众号的appId]，通过appId构造不同的AppSetting
+     *
      * @param signature
      * @param msg_signature
      * @param timestamp
@@ -147,26 +172,24 @@ public class WxCallbackController {
     }
 
     private XmlMessageHeader mpDispatch(XmlMessageHeader xmlRequest) {
-        String reply = "您好，正在为您接入客服。";
         if(!duplicatedMessageChecker.isDuplicated(xmlRequest.getFromUser() + xmlRequest.getCreateTime().getTime())) {
+            String welcome = "您好:" + Users.defaultUsers().get(xmlRequest.getFromUser()).getNickName();
+            CareMessages.defaultCareMessages().text(xmlRequest.getFromUser(), welcome);
 
-            if(xmlRequest instanceof ClickEvent) {
-                ClickEvent clickEvent = (ClickEvent)xmlRequest;
-                if("contact".equals(clickEvent.getEventKey())) {
-                    Forward2CareXmlMessage kfMessage = new Forward2CareXmlMessage();
-                    kfMessage.setFromUser(xmlRequest.getToUser());
-                    kfMessage.setToUser(xmlRequest.getFromUser());
-                    kfMessage.setCreateTime(xmlRequest.getCreateTime());
-                    return kfMessage;
-                }
+            if (xmlRequest instanceof EventRequest) {
+                EventRequest eventRequest = (EventRequest) xmlRequest;
+                logger.debug("事件请求[{}]", eventRequest.getEventType().name());
+                CareMessages.defaultCareMessages().text(xmlRequest.getFromUser(), "事件请求:" + eventRequest.getEventType().name());
+            } else {
+                logger.debug("消息请求[{}]", xmlRequest.getMsgType().name());
+                CareMessages.defaultCareMessages().text(xmlRequest.getFromUser(), "消息请求:" + xmlRequest.getMsgType().name());
             }
-            TextXmlMessage textXmlMessage = new TextXmlMessage();
-            textXmlMessage.content(reply).toUser(xmlRequest.getFromUser()).fromUser(xmlRequest.getToUser());
-            return textXmlMessage;
         } else {
             logger.warn("Duplicated message: {} @ {}", xmlRequest.getMsgType(), xmlRequest.getFromUser());
         }
 
+        //需要同步返回消息（被动回复）给用户则构造一个XmlMessageHeader类型，比较鸡肋，因为处理逻辑如果比较复杂响应太慢会影响用户感知，建议直接返回null；
+        //要发送消息给用户可以参考上面的例子使用客服消息接口进行异步发送
         return null;
     }
 
