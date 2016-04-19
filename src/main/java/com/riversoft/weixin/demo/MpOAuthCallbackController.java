b735f5b73aebb51b3399326b7ea456d8c82e244c
borball <borball.zhai@ericsson.com>
2016-04-19 12:11:31 -0400

    update sdk to 0.8.2

---
+++ src/main/java/com/riversoft/weixin/demo/MpOAuthCallbackController.java
@@ -0,0 +1,29 @@
+package com.riversoft.weixin.demo;
+
+import com.riversoft.weixin.common.oauth2.AccessToken;
+import com.riversoft.weixin.common.oauth2.OpenUser;
+import com.riversoft.weixin.mp.oauth2.MpOAuth2s;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by exizhai on 12/17/2015.
+ */
+public class MpOAuthCallbackController {
+
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(WxCallbackController.class);
+
+    /**
+     * 公众号OAuth回调接口
+     * @return
+     */
+    @RequestMapping("/wx/oauth/mp")
+    public void mp(@RequestParam(value="code") String code, @RequestParam(value="state", required = false) String state) {
+
+        logger.info("code:{}, state:{}", code, state);
+        AccessToken accessToken = MpOAuth2s.defaultOAuth2s().getAccessToken(code);
+        OpenUser openUser = MpOAuth2s.defaultOAuth2s().userInfo(accessToken.getAccessToken(), accessToken.getOpenId());
+        //do your logic
+    }
+}
